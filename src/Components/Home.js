c459347573a7561a2a8317d508f0e1d960d49dc9
amaysharma20 <amaysharma82530@gmail.com>
2021-04-16 22:05:03 +0530

    Landing Page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -137,10 +137,6 @@ function Home(){
       src={slack4}
       alt="First slide"
     />
-    <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
     <img
@@ -148,11 +144,6 @@ function Home(){
       src={slack5}
       alt="Second slide"
     />
-
-    <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
     <img
@@ -160,11 +151,6 @@ function Home(){
       src={slack7}
       alt="Third slide"
     />
-
-    <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-    </Carousel.Caption>
   </Carousel.Item>
 </Carousel>
 </Row>
